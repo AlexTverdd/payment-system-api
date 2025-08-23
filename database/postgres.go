7ded419424ff64a84e67912ef938e234a9df3684
AlexTverdd <tverdohlebov00@yandex.ru>
2025-08-23 14:09:13 +0700

    feat: добавлена функция InitialSetup для создание кошельков если их нет

--- database/postgres.go
+++ database/postgres.go
@@ -31,7 +31,7 @@ func Migrate() {
 	log.Println("Миграция базы данных успешна")
 }
 
-// generateWalletAddress генерирует адресс
+// generateWalletAddress генерирует адрес
 func generateWalletAddress() (string, error) {
 	bytes := make([]byte, 32)
 
@@ -40,4 +40,29 @@ func generateWalletAddress() (string, error) {
 		return "", fmt.Errorf("ошибка при чтении случайных байт: %w", err)
 	}
 	return hex.EncodeToString(bytes), nil
+}
+
+func InitialSetup() {
+	var count int64
+	DB.Model(&Wallet{}).Count(&count)
+	if count == 0 {
+		fmt.Println("Создание начальных кошельков")
+		wallets := make([]Wallet, 10)
+		for i := 0; i < 10; i++ {
+			address, err := generateWalletAddress()
+			if err != nil {
+				log.Fatalf("Не удалось сгенерировать адрес кошелька: %v", err)
+			}
+			wallets[i] = Wallet{
+				Address: address,
+				Balance: 100.0,
+			}
+		}
+		if err := DB.Create(&wallets).Error; err != nil {
+			log.Fatalf("Не удалось создать начальные кошельки: %v", err)
+		}
+		fmt.Println("10 начальных кошельков созданы")
+	} else {
+		fmt.Println("Кошельков уже существует", count)
+	}
 }
